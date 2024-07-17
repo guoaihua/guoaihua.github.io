3eb9d66afc03cde6dd374e6d0ae086ebfe6f0dfa
zimingguo <zimingguo@tencent.com>
2024-07-17 19:48:04 +0800

    feat: update index

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const static = require('koa-static')
 const path = require('path')
 
 app.use(static(
-    path.join( __dirname,  './public')
+    path.join( __dirname,  './docs')
 ))
 
   app.listen(4000, () => {
-    console.log('[demo] static-use-middleware is starting at port 3000')
+    console.log('[demo] static-use-middleware is starting at port 4000')
   })
